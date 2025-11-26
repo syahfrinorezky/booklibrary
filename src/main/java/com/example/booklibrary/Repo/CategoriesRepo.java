ef3c19a47eb001942e60a716e80a573606fc003b
rinorezky <syahfrinorezky@gmail.com>
2025-11-26 20:55:19 +0800

    feat: add categories repo

---
+++ src/main/java/com/example/booklibrary/Repo/CategoriesRepo.java
@@ -0,0 +1,13 @@
+package com.example.booklibrary.Repo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.booklibrary.Model.Categories;
+
+public interface CategoriesRepo extends JpaRepository<Categories, Long> {
+    List<Categories> findAllByDeletedAtIsNull();
+    Optional<Categories> findByIdAndDeletedAtIsNull(Long id);
+}
