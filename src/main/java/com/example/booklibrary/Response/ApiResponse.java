b2fdc009ce235345cb123a6bf2bf6b57fb10f165
rinorezky <syahfrinorezky@gmail.com>
2025-11-26 16:30:57 +0800

    feat: add api response helper

---
+++ src/main/java/com/example/booklibrary/Response/ApiResponse.java
@@ -0,0 +1,39 @@
+package com.example.booklibrary.Response;
+
+public class ApiResponse {
+    public String status;
+    public String message;
+    public Object data;
+
+    public ApiResponse() {}
+
+    public ApiResponse(String status, String message, Object data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
