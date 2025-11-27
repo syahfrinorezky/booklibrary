1ff3f85012de4a95adf91c45336eeed10423339d
rinorezky <syahfrinorezky@gmail.com>
2025-11-27 08:35:54 +0800

    feat: add book repo

---
+++ src/main/java/com/example/booklibrary/Repo/BooksRepo.java
@@ -0,0 +1,13 @@
+package com.example.booklibrary.Repo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.booklibrary.Model.Books;
+
+public interface BooksRepo extends JpaRepository<Books, Long> {
+    List<Books> findAllByDeletedAtIsNull();
+    Optional<Books> findByIdAndDeletedAtIsNull(Long id);
+}
