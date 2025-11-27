50c8984be0b87536f3e24ffe3eed5e580f6f1d5a
rinorezky <syahfrinorezky@gmail.com>
2025-11-27 09:07:08 +0800

    chore: add indexing for make it query easier & adjust for several column

--- src/main/resources/db/migration/V1__initial.sql
+++ src/main/resources/db/migration/V1__initial.sql
@@ -9,6 +9,8 @@ CREATE TABLE
         deleted_at TIMESTAMP NULL
     );
 
+CREATE INDEX idx_members_deleted_at ON members (deleted_at);
+
 CREATE TABLE
     categories (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -19,30 +21,45 @@ CREATE TABLE
         deleted_at TIMESTAMP NULL
     );
 
+CREATE INDEX idx_categories_deleted_at ON categories (deleted_at);
+
 CREATE TABLE
     books (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
-        category_id BIGINT,
+        category_id BIGINT NULL,
         title VARCHAR(100) NOT NULL,
         author VARCHAR(100) NOT NULL,
         publisher VARCHAR(100) NOT NULL,
-        year YEAR,
+        year INT (4),
         stock INT NOT NULL DEFAULT 0,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         deleted_at TIMESTAMP NULL,
-        CONSTRAINT fk_book_category FOREIGN KEY (category_id) REFERENCES categories (id)
+        CONSTRAINT fk_book_category FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE SET NULL
     );
+
+CREATE INDEX idx_books_category_id ON books (category_id);
+
+CREATE INDEX idx_books_deleted_at ON books (deleted_at);
 
 CREATE TABLE
     loans (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
-        member_id BIGINT NOT NULL,
-        book_id BIGINT NOT NULL,
+        member_id BIGINT NULL,
+        book_id BIGINT NULL,
         loan_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         due_date TIMESTAMP NULL,
         return_date TIMESTAMP NULL,
         status ENUM ('LOANED', 'RETURNED', 'LATE', 'LOST') DEFAULT 'LOANED',
-        CONSTRAINT fk_loan_member FOREIGN KEY (member_id) REFERENCES members (id),
-        CONSTRAINT fk_loan_book FOREIGN KEY (book_id) REFERENCES books (id)
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+        deleted_at TIMESTAMP NULL,
+        CONSTRAINT fk_loan_member FOREIGN KEY (member_id) REFERENCES members (id) ON DELETE SET NULL,
+        CONSTRAINT fk_loan_book FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE SET NULL
     );
+
+CREATE INDEX idx_loans_member_id ON loans (member_id);
+
+CREATE INDEX idx_loans_book_id ON loans (book_id);
+
+CREATE INDEX idx_loans_deleted_at ON loans (deleted_at);
